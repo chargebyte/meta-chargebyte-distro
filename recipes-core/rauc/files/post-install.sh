6e40e2b0808b7440b6885f17b7b0ce363bf8da19
Simon Stürz <simon.stuerz@chargebyte.com>
2026-01-29 09:36:28 +0100

    RAUC: Add conditional post-install handler for an automatic system reboot
    Signed-off-by: Simon Stürz <simon.stuerz@chargebyte.com>

--- recipes-core/rauc/files/post-install.sh
+++ recipes-core/rauc/files/post-install.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
+# Trigger a reboot after the bundle finished installing, if the file
+# /run/firmware-update-reboot-requested exists, otherwise do nothing.
+#
+# If the update has been performed by a service which might be already stopped
+# due to configuration sync, the reboot can be automatically triggered once the
+# installation process has finished successfully.
+
+REBOOT_REQUESTED_FILE=/run/firmware-update-reboot-requested
+if [ ! -f "$REBOOT_REQUESTED_FILE" ]; then
+    exit 0
+fi
+
+logger -t rauc-post-install "Reboot has been requested using $REBOOT_REQUESTED_FILE file"
+sync
+
+# Prefer systemd reboot if available
+if command -v systemctl >/dev/null 2>&1; then
+	systemctl reboot
+else
+	reboot
+fi
+
 exit 0
